e8792023a10fbc62c8083ce527ea9def0a035c0a
Eugene Kim <eugenehkim25@gmail.com>
2022-09-22 21:41:57 -0600

    Rudimentary beginnings for login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
-  {path: "about", component: AboutComponent}
+  {path: "about", component: AboutComponent},
+  {path: "login", component: LoginComponent}
 ];
 
 @NgModule({
