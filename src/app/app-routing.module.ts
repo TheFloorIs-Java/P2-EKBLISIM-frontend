b91c092740650eb083ccd3b92e4a2938b939b5ed
Eugene Kim <eugenehkim25@gmail.com>
2022-09-29 21:39:30 -0600

    Added routing guards and admin portal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AdminloginComponent } from './pages/adminlogin/adminlogin.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { PackageComponent } from './components/package/package.component';
+import { AuthGuard } from './auth/auth.guard';
+import { AdminComponent } from './admin/admin/admin.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'admin', component: AdminloginComponent },
+  { path: 'adminlogin', component: AdminloginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'packages', component: PackageComponent },
+  { path: 'packages', component: PackageComponent, canActivate: [AuthGuard] },
+  { path: 'adminportal', component: AdminComponent, canActivate: []}
 ];
 
 @NgModule({
