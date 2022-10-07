000dc2c865c858c7ed33290b26cc4e9c4650912f
Ilma Shaharin <shaharinilma96@gmail.com>
2022-10-07 13:25:51 -0400

    login minor change

--- src/app/react-login/components/loginform/loginform.component.ts
+++ src/app/react-login/components/loginform/loginform.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
 import { Account } from 'src/app/model/Account';
@@ -9,58 +14,82 @@ import { LoginAuthService } from '../../services/login-auth.service';
 @Component({
   selector: 'app-loginform',
   templateUrl: './loginform.component.html',
-  styleUrls: ['./loginform.component.css']
+  styleUrls: ['./loginform.component.css'],
 })
 export class LoginformComponent implements OnInit {
-
   form: FormGroup;
-  constructor(private readonly formBuilder: FormBuilder,
-              private authService: LoginAuthService,
-              private tservice: TravelServiceService,
-              private router: Router) { 
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private authService: LoginAuthService,
+    private tservice: TravelServiceService,
+    private router: Router
+  ) {
     this.form = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
-      userPassword: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12), this.validatePassword]]
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(15),
+        ],
+      ],
+      userPassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(12),
+          this.validatePassword,
+        ],
+      ],
     });
   }
 
   userAccounts?: Account[];
   userAccount?: Account;
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   validatePassword(fc: FormControl) {
     // const value = fc.value as string;
     // const isInvalid = 'pa'
   }
 
-  get controls() { return this.form.controls; }
-
+  get controls() {
+    return this.form.controls;
+  }
 
   submitForm(): void {
     if (this.form.valid) {
-      this.tservice.getUserLoginInput().subscribe(userAccounts => this.userAccounts = userAccounts); // First get the list of users from backend.
+      this.tservice
+        .getUserLoginInput()
+        .subscribe((userAccounts) => (this.userAccounts = userAccounts)); // First get the list of users from backend.
       console.log(this.userAccounts);
-      if (this.userAccounts?.find((user) => { // Then check if user is in that list.
-        // return (this.authService.loginAuthenticated("melogin", this.controls['username'].value, 
-        //                                             "mepassword", this.controls['userPassword'].value));
-        return (this.authService.loginAuthenticated(user.username, this.controls['username'].value, 
-                                                    user.password, this.controls['userPassword'].value));
-      }) !== null) { // In this case, if user DOES exist, sign user in.
-        console.log("Successful user login");
-        sessionStorage.setItem('signedIn', 'true');
-        sessionStorage.setItem('token', this.controls['username'].value);
+      if (
+        this.userAccounts?.find((user) => {
+          // Then check if user is in that list.
+          // return (this.authService.loginAuthenticated("melogin", this.controls['username'].value,
+          //                                             "mepassword", this.controls['userPassword'].value));
+          return this.authService.loginAuthenticated(
+            user.username,
+            this.controls['username'].value,
+            user.password,
+            this.controls['userPassword'].value
+          );
+        }) !== undefined
+      ) {
+        // In this case, if user DOES exist, sign user in.
+        console.log('Successful user login');
+        localStorage.setItem('signedIn', 'true');
+        localStorage.setItem('token', this.controls['username'].value);
         // this.tservice.postUserLoginInput(this.controls['username'].value, this.controls['userPassword'].value);
         // console.log(this.form.getRawValue());
         this.router.navigate(['/packages']); // After user is verified to exist in database.
       } else {
         console.log("This user doesn't exists");
       }
-    }
-    else {
+    } else {
       console.log('Input not verified');
     }
   }
-
 }
--- src/app/react-login/components/register-form/register-form.component.ts
+++ src/app/react-login/components/register-form/register-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { Account } from 'src/app/model/Account';
 import { TravelServiceService } from 'src/app/service/travel-service.service';
@@ -8,22 +13,38 @@ import { LoginAuthService } from '../../services/login-auth.service';
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.css']
+  styleUrls: ['./register-form.component.css'],
 })
 export class RegisterFormComponent implements OnInit {
-
   userAlreadyExists: boolean = false;
 
   userAccounts: Array<Account> = [];
 
   form: FormGroup;
-  constructor(private readonly formBuilder: FormBuilder,
-              private authService: LoginAuthService,
-              private tservice: TravelServiceService,
-              private router: Router) { 
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private authService: LoginAuthService,
+    private tservice: TravelServiceService,
+    private router: Router
+  ) {
     this.form = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
-      userPassword: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12), this.validatePassword]]
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(15),
+        ],
+      ],
+      userPassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(12),
+          this.validatePassword,
+        ],
+      ],
     });
   }
 
@@ -32,34 +53,47 @@ export class RegisterFormComponent implements OnInit {
     // const isInvalid = 'pa'
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  get controls() { return this.form.controls; }
+  get controls() {
+    return this.form.controls;
+  }
 
   submitForm(): void {
     this.userAlreadyExists = false;
     if (this.form.valid) {
-      this.tservice.getUserLoginInput().subscribe(userAccounts => this.userAccounts = userAccounts); // First get the list of users from backend.
+      this.tservice
+        .getUserLoginInput()
+        .subscribe((userAccounts) => (this.userAccounts = userAccounts)); // First get the list of users from backend.
       console.log(this.userAccounts);
-      if (this.userAccounts.find((user) => { // Then check if user is in that list.
-        // return (this.authService.loginAuthenticated("melogin", this.controls['username'].value, 
-        //                                             "mepassword", this.controls['userPassword'].value));
-        return (this.authService.loginAuthenticated(user.username, this.controls['username'].value, 
-                                                    user.password, this.controls['userPassword'].value));
-      }) === null) { // If user doesn't exist, post the new user and automatically sign user in.
-        console.log("Successful user creation");
-        sessionStorage.setItem('signedIn', 'true');
-        sessionStorage.setItem('token', this.controls['username'].value);
-        this.tservice.postUserLoginInput(this.controls['username'].value, this.controls['userPassword'].value);
+      if (
+        this.userAccounts.find((user) => {
+          // Then check if user is in that list.
+          // return (this.authService.loginAuthenticated("melogin", this.controls['username'].value,
+          //                                             "mepassword", this.controls['userPassword'].value));
+          return this.authService.loginAuthenticated(
+            user.username,
+            this.controls['username'].value,
+            user.password,
+            this.controls['userPassword'].value
+          );
+        }) === undefined
+      ) {
+        // If user doesn't exist, post the new user and automatically sign user in.
+        console.log('Successful user creation');
+        localStorage.setItem('signedIn', 'true');
+        localStorage.setItem('token', this.controls['username'].value);
+        this.tservice.postUserLoginInput(
+          this.controls['username'].value,
+          this.controls['userPassword'].value
+        );
         // console.log(this.form.getRawValue());
         this.router.navigate(['/packages']); // After user is verified to exist in database.
       } else {
-        console.log("The user already exists");
+        console.log('The user already exists');
         this.userAlreadyExists = true;
       }
-    }
-    else {
+    } else {
       console.log('Input not verified');
     }
   }
